aa7f58751e29fd75b74936c53a939faf1ee868ac
Prasanna Pattam <pprasannak@hotmail.com>
2015-06-28 00:33:04 +0000

    Changed the data types for Ids
    git-tfs-id: [https://prasannapattam.visualstudio.com/DefaultCollection]$/Mine;C48

--- eMine/src/eMine/Lib/Entities/BaseEntity.cs
+++ eMine/src/eMine/Lib/Entities/BaseEntity.cs
@@ -7,18 +7,18 @@ namespace eMine.Lib.Entities
 {
     public class BaseEntity
     {
-        public int CreatedUserId { get; set; }
+        public string CreatedUserId { get; set; }
         public DateTime CreatedDate { get; set; }
-        public int? LastModifiedUserId { get; set; }
+        public string LastModifiedUserId { get; set; }
         public DateTime? LastModifiedDate { get; set; }
         public bool DeletedInd { get; set; }
         public int CompanyId { get; set; }
 
         public BaseEntity()
         {
-            CreatedUserId = 0;  // TODO: change this to get the current logged in user
+            CreatedUserId = "prasanna@nootus.com";  // TODO: change this to get the current logged in user
             CreatedDate = DateTime.UtcNow;
-            LastModifiedUserId = 0;
+            LastModifiedUserId = "prasanna@nootus.com";
             LastModifiedDate = DateTime.UtcNow;
             DeletedInd = false;
             CompanyId = 1;
@@ -27,7 +27,7 @@ namespace eMine.Lib.Entities
         public virtual void UpdateAuditFields()
         {
             LastModifiedDate = DateTime.Now;
-            LastModifiedUserId = 0;  //TODO: Change this get the current user
+            LastModifiedUserId = "prasanna@nootus.com";  //TODO: Change this get the current user
         }
     }
 }
