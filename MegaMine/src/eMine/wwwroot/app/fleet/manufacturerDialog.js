486bfb82f464c5f8fdbca2faee153571479a447d
Prasanna Pattam <pprasannak@hotmail.com>
2015-09-15 06:22:44 +0530

    Adding the ManufacturerDialog files

---
+++ MegaMine/src/eMine/wwwroot/app/fleet/manufacturerDialog.js
@@ -0,0 +1,34 @@
+﻿'use strict'
+angular.module('emine').factory('manufacturerDialog', manufacturerDialog);
+manufacturerDialog.$inject = ['dialogService', 'vehicleService', 'utility'];
+
+function manufacturerDialog(dialogService, vehicleService, utility) {
+
+    var dialog =
+    {
+        viewDialog: viewDialog,
+    };
+
+    return dialog;
+
+    function viewDialog(model, dialogMode, ev) {
+        dialogService.show({
+            templateUrl: utility.virtualDirectory + '/app/fleet/manufacturerDialog.html',
+            targetEvent: ev,
+            data: { model: model },
+            dialogMode: dialogMode
+        })
+        .then(function (dialogModel) {
+            vehicleService.saveManufacturer(dialogModel).then(function () {
+                if (model.vehicleManufacturerId === 0) {
+                    vehicleService.getManufacturerList();
+                }
+                else {
+                    model.name = dialogModel.name
+                    model.description = dialogModel.description
+                }
+            });
+            dialogService.hide();
+        });
+    }
+}
