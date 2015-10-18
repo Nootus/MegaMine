ec92c420e5d473eb0f64bc9937e7fd25282418b2
Prasanna Pattam <pprasannak@hotmail.com>
2015-10-18 09:18:20 +0530

    Added DB Project

---
+++ MegaMine/src/eMine/Models/Quarry/SummaryModel.cs
@@ -0,0 +1,13 @@
+﻿using eMine.Lib.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eMine.Models.Quarry
+{
+    public class SummaryModel
+    {
+        public List<ProductTypeModel> ProductTypes { get; set; }
+    }
+}
