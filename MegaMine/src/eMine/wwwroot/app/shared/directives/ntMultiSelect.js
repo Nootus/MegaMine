1596d028508d5f7d8ea4866d5e8da691d833094d
Prasanna Pattam <pprasannak@hotmail.com>
2015-11-14 19:57:27 +0530

    Upgraded javascript libraries and removed Kendo

--- MegaMine/src/eMine/wwwroot/app/shared/directives/ntMultiSelect.js
+++ MegaMine/src/eMine/wwwroot/app/shared/directives/ntMultiSelect.js
@@ -1,46 +1,37 @@
 ﻿'use strict';
 angular.module('emine').directive('ntMultiSelect', ntMultiSelect)
-ntMultiSelect.$inject = ['$mdDialog', 'constants'];
+ntMultiSelect.$inject = [];
 
-function ntMultiSelect($mdDialog, constants) {
+function ntMultiSelect() {
     return {
-        restrict: 'E',
-        transclude: true,
+        restrict: 'E',  
         scope: {
-            form: '@',
-            title: '@',
-            saveText: '@'
+            ngModel: "=",
+            ntSelectedItem: "=",
+            ntItems: '=',
+            ntItemText: '=',
         },
         link: link,
-        template: ''
-                    + '<form name="{{form}}" novalidate>'
-                    + '    <em-toolbar title="{{title}}" class="dialog">'
-                    + '       <nt-button ng-click="save(dialogForm)" button-icon="save" button-text="{{saveText}}" ng-show="dialogMode === dialogModeEnum.save" ng-disabled="dialogForm.$invalid && dialogForm.$submitted""></nt-button>'
-                    + '       <nt-button ng-click="deleteItem($event)" button-icon="delete" button-text="Delete" ng-show="dialogMode === dialogModeEnum.delete"></nt-button>'
-                    + '       <nt-button ng-click="cancel($event)" button-icon="cancel" button-text="Cancel" ng-show="dialogMode !== dialogModeEnum.view"></nt-button>'
-                    + '       <nt-button ng-click="cancel($event)" button-icon="cancel" button-text="Close" ng-show="dialogMode === dialogModeEnum.view"></nt-button>'
-                    + '    </em-toolbar>'
-                    + '    <md-dialog-content class="dialog-content">'
-                    + '       <div ng-transclude></div>'
-                    + '    </md-dialog-content>'
-                    + '</form>'
-                    + ''
+        template: '<md-chips ng-model="ngModel"'
+                    + ' md-require-match="true">'
+                        + '<md-autocomplete md-selected-item="ntSelectedItem"'
+                        + ' md-items="item in ntItems"'
+                        + ' md-item-text="item"'
+                        + ' md-min-length="0"'
+                        + '>'
+                            + '<md-item-template>'
+                                + '<span>{{item}}</span>'
+                            + '</md-item-template>'
+                        + '</md-autocomplete>'
+                        + '<md-chip-template>'
+                            + '<span>'
+                            + '<strong>{{$chip}}</strong>'
+                            + '</span>'
+                        + '</md-chip-template>'
+                    + '</md-chips>'
     };
 
     function link(scope, element, attrs, nullController, transclude) {
-        //interal variables
-        angular.extend(scope, {
-            dialogModeEnum: constants.enum.dialogMode,
-            dialogMode: scope.$parent.dialogMode,
-            save: scope.$parent.save,
-            deleteItem: scope.$parent.deleteItem,
-            cancel: scope.$parent.cancel,
-        });
 
-        if (scope.saveText === undefined)
-            scope.saveText = 'Save';
-
-        scope.dialogForm = scope[scope.form]
-        scope.$parent.dialogForm = scope.dialogForm;
     }
 }
