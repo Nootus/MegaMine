462e8ba44d547f429bebc1a316ffcf041131619b
Prasanna Pattam <pprasannak@hotmail.com>
2016-01-12 18:54:54 +0530

    Added the page claim for non primary page for material colour

--- MegaMine/src/MegaMineDB/Scripts/ChangesMasterData.sql
+++ MegaMine/src/MegaMineDB/Scripts/ChangesMasterData.sql
@@ -1,4 +1,13 @@
-﻿-- Quarry Delete
+﻿-- Not primary claim for Material Colour (being called from QuarryView)
+if not exists(select 1 from IdentityPageClaim where id = 171)
+begin
+	set identity_insert IdentityPageClaim on
+	insert into IdentityPageClaim(id, PageId, ClaimId, PrimaryClaimInd) values(171, 57, 47, 0)
+	set identity_insert IdentityPageClaim off
+end
+-- end
+
+-- Quarry Delete
 if not exists(select 1 from IdentityClaim where id = 1)
 begin
 	insert into IdentityClaim(id, ClaimType, ClaimValue, Description) values(62, 'Quarry', 'QuarryDelete', 'Quarry Delete')
