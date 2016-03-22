01bc52cfcfab639442a4388a9328070566f23e4f
Prasanna Pattam <pprasannak@hotmail.com>
2016-03-22 06:19:42 +0530

    Implemented sample charts in Quarries

--- MegaMine/src/MegaMine.Web/wwwroot/app/app.js
+++ MegaMine/src/MegaMine.Web/wwwroot/app/app.js
@@ -1,7 +1,7 @@
 /// <reference path="../../typings/tsd.d.ts" />
 var megamime;
 (function (megamime) {
-    angular.module('megamine', ['ngAnimate', 'ngMessages', 'ngMaterial', 'ui.router', 'ui.grid', 'ui.grid.resizeColumns', 'ui.grid.autoResize', 'ui.grid.exporter', 'ui.grid.selection', 'ui.grid.edit', 'ui.grid.rowEdit', 'ui.grid.cellNav', 'ui.grid.validate']);
+    angular.module('megamine', ['ngAnimate', 'ngMessages', 'ngMaterial', 'ui.router', 'ui.grid', 'ui.grid.resizeColumns', 'ui.grid.autoResize', 'ui.grid.exporter', 'ui.grid.selection', 'ui.grid.edit', 'ui.grid.rowEdit', 'ui.grid.cellNav', 'ui.grid.validate', 'nvd3']);
 })(megamime || (megamime = {}));
 
 //# sourceMappingURL=app.js.map
