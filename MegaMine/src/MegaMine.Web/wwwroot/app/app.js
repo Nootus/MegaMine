1e3b035626c6d7912a370ef5fa322c4c522d4e8d
Prasanna Pattam <pprasannak@hotmail.com>
2016-04-04 06:50:28 +0530

    Moved Security project under Services

--- MegaMine/src/MegaMine.Web/wwwroot/app/app.js
+++ MegaMine/src/MegaMine.Web/wwwroot/app/app.js
@@ -3,5 +3,4 @@ var megamime;
 (function (megamime) {
     angular.module('megamine', ['ngAnimate', 'ngMessages', 'ngMaterial', 'ui.router', 'ui.grid', 'ui.grid.resizeColumns', 'ui.grid.autoResize', 'ui.grid.exporter', 'ui.grid.selection', 'ui.grid.edit', 'ui.grid.rowEdit', 'ui.grid.cellNav', 'ui.grid.validate', 'nvd3', 'gridster']);
 })(megamime || (megamime = {}));
-
 //# sourceMappingURL=app.js.map
