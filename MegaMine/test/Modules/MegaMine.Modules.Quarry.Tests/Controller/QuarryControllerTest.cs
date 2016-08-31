70e86dd281d8429cbb2b2c50b4a4ab1b808a9a4d
Prasanna Pattam <pprasannak@hotmail.com>
2016-08-31 07:24:25 +0530

    Added Test project

---
+++ MegaMine/test/Modules/MegaMine.Modules.Quarry.Tests/Controller/QuarryControllerTest.cs
@@ -0,0 +1,13 @@
+﻿namespace MegaMine.Modules.Quarry.Tests.Controller
+{
+    using Xunit;
+
+    public class QuarryControllerTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
---
+++ MegaMine/test/Modules/MegaMine.Modules.Quarry.Tests/Program.cs
@@ -0,0 +1,14 @@
+﻿using MegaMine.Modules.Quarry.Tests.Controller;
+
+namespace MegaMine.Modules.Quarry.Tests
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            QuarryControllerTest tst = new QuarryControllerTest();
+
+            tst.Test1();
+        }
+    }
+}
